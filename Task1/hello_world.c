9a3cbede5268505d97df1f5d53d813b43839ca1a
mshan <mohsinshan1k@gmail.com>
2016-11-12 20:48:18 +0500

    Corrected Error. Print to Kern Debug log level.

--- Task1/hello_world.c
+++ Task1/hello_world.c
@@ -1,19 +1,23 @@
 #include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/kernel.h> 
 
 #define DRIVER_AUTHOR "Mohsin Shan"
 #define DRIVER_DESC "A simple driver that prints Hello World"
 
 int init_module(void)
 {
-	pr_debug("Hello World\n");
+	
+	printk(KERN_DEBUG "Hello World\n");
 	return 0;
+
+
 }
 
 
 void cleanup_module(void)
 {
-	pr_debug("Goodbye World\n");
+	printk(KERN_DEBUG "Goodbye World\n");
+
 }
 
 MODULE_LICENSE("GPL");
